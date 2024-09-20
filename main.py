4569fb4cd098d17a129c50f87e372d3de6f89701
Seyed Saeed Mohammadi <49374230+saeed090@users.noreply.github.com>
2024-09-20 19:19:11 +0330

    commit
    commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import  cv2
+import  mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+
+while True:
+    success, img = cap.read()
+
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for hand in results.multi_hand_landmarks:
+            mp.solutions.drawing_utils.draw_landmarks(img,hand,mpHands.HAND_CONNECTIONS)
+
+
+    cv2.imshow('Image', img)
+    cv2.waitKey(1)
